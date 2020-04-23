ed43e285d5f6fe38c0f137d97e74ca89d0ea2d09
Brian Leip <brianleip@gmail.com>
2020-04-22 23:44:39 -0700

    Finished PenguinArea.cs

---
+++ MLAgentPenguins/Assets/Penguin/Scripts/PenguinAgent.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using MLAgents;
+
+public class PenguinAgent : Agent
+{
+    [Tooltip("How fast the agent moves forward")]
+    public float moveSpeed = 5f;
+
+    [Tooltip("How fast the agent turns")]
+    public float turnSpeed = 180f;
+
+    [Tooltip("Prefab of the heart that appears when the baby is fed")]
+    public GameObject heartPrefab;
+
+    [Tooltip("Prefab of the regurgitated fish that appears when the baby is fed")]
+    public GameObject regurgitatedFishPrefab;
+
+    private PenguinArea penguinArea;
+    new private Rigidbody rigidbody;
+    private GameObject baby;
+    private bool isFull; // If true, penguin has a full stomach
+    private float feedRadius = 0f;
+
+    /// <summary>
+    /// Initial setup, called when the agent is enabled
+    /// </summary>
+    public override void InitializeAgent() {
+        base.InitializeAgent();
+        penguinArea = GetComponentInParent<PenguinArea>();
+        baby = penguinArea.penguinBaby;
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    
+}
